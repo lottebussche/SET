5dab3e287d3af6eab9f84fb9be3dbba96dc50675
Malouth <m.a.terhorst@students.uu.nl>
2024-06-15 10:58:20 +0200

    Update display_of_cards_Julia.py

--- display_of_cards_Julia.py
+++ display_of_cards_Julia.py
@@ -6,8 +6,8 @@ import random
 pygame.init()
 
 # Determine the screen 
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("SET: Malu ter Horst, Lotte Bussche, Laura Mercenier, Julia van Oostrom")
+screen = pygame.display.set_mode((500, 700))
+pygame.display.set_caption("SET: Malou ter Horst, Lotte Bussche, Laura Mercenier, Julia van Oostrom")
 
 # Card attributes
 color = ['red', 'green', 'purple']
@@ -27,16 +27,30 @@ card_images = {name: pygame.image.load(f'kaarten/{name}.gif') for name in list_o
 
 # Determine positions to display some cards
 display_cards = random.sample(list_of_81_cards, 12)
+print(display_cards)
 
 # Hier denk ik dat het probleem zit, we moeten iets doen dat vanaf kaart 4 en 8 de kaarten lager komen.
-# card_positions = []
+card_positions = []
+for i in range(0, 4):
+    place = [10 + i * 110, 10]
+    card_positions.append(place)
+for i in range(0, 4):
+    place = [10 + i * 110, 225]
+    card_positions.append(place)
+for i in range(0, 4):
+    place = [10 + i * 110, 440]
+    card_positions.append(place)
+#card_positions.append(10 + i * 110, 50 for i in display_cards[:4])
+#card_positions.append(10 + i * 110, 150 for i in display_cards[4:9])
+#card_positions.append(10 + i * 110, 250 for i in display_cards[9:13])
+print(card_positions)
 #for i in range(5):
     #card_positions.append ((40 + i * 110, 100))
 #for i in range (5-11): 
     #card_positions.append ((40 + (i-5) * 110, 300))
 
 #Hierdoor print je wel wat maar alles op een rij:
-card_positions = [(10 + i * 110, 100) for i in range(len(display_cards))]
+#card_positions = [(10 + i * 110, 50) for i in range(len(display_cards) // 4)]
 
 # Game loop, neccesery to end the game
 running = True
